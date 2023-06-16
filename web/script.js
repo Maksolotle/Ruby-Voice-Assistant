d5fd302908305892b961f6cbe078e5b2c1bef73f
Maks <136234364+Maksolotle@users.noreply.github.com>
2023-06-16 20:02:36 +0300

    Update script.js

--- web/script.js
+++ web/script.js
@@ -5,40 +5,7 @@ jQuery('#but_2').on('click', function(){
 	
 		jQuery('#all').hide();
 		jQuery('#options').show();
-
-
-		jQuery('#white').on('click', function(){
-			
-			document.body.style.backgroundImage = 'url(assets/media/background_white.png)';
-			document.body.style.color = 'black';
-
-		});
-
-		jQuery('#black').on('click', function(){
-			
-			document.body.style.backgroundImage = 'url(assets/media/background.png)';
-			document.body.style.color = 'white';
-
-
-		});
-
-		jQuery('#red').on('click', function(){
-
-			document.body.style.backgroundImage = 'url(assets/media/not_standart_temas/red.png)';
-			document.body.style.color = 'white';
-		});
-
-		jQuery('#green').on('click', function(){
-
-			document.body.style.backgroundImage = 'url(assets/media/not_standart_temas/green.png)';
-			document.body.style.color = 'white';
-		});
-
-		jQuery('#violet').on('click', function(){
-
-			document.body.style.backgroundImage = 'url(assets/media/not_standart_temas/violet.png)';
-			document.body.style.color = 'white';
-
+	
 		});
 
 		jQuery('#nazad').on('click', function(){
